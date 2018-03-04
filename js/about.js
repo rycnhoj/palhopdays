d7d1d749963249049b13ef410d0374d0fb0482db
John Cyr <rycnhoj@gmail.com>
2018-03-04 00:10:09 -0500

    - Added navbar and about page

---
+++ js/about.js
@@ -0,0 +1,23 @@
+// var $ = require('jquery')
+// var siteText = require('../../../data/siteText')
+
+// function about (params) {
+//   return _buildAbout()
+// }
+
+// function _buildTitle (title) {
+//   return '<h3 class="page-title">' + title + '</h3>'
+// }
+
+// function _buildAbout () {
+//   var aboutSection = $('.about-section')
+//   var title = _buildTitle('About')
+//   var video = '<iframe id="ytplayer" type="text/html" width="720" height="405" src="https://www.youtube.com/embed/dFd5yPSsXV0?controls=0&rel=0&showinfo=0" frameborder="0" allowfullscreen>'
+
+//   console.log(siteText['royalKnights'])
+
+//   aboutSection.append(title)
+//   aboutSection.append(video)
+// }
+
+// module.exports = about
---
+++ js/navbar.js
@@ -0,0 +1,33 @@
+// function navbar () {
+//   return _buildNavbar()
+// }
+
+// function _buildNavbar () {
+//   var title = 'Pal Hop Days'
+//   var links = {
+//     'about': '<li><a href="#">About</a></li>',
+//     'bands': '<li><a href="#">Bands</a></li>',
+//     'contact': '<li><a href="#">Contact</a></li>',
+//     'shop': '<li><a href="#">Shop</a></li>',
+//     'sponsorship': '<li><a href="#">Sponsorship</a></li>'
+//   }
+
+//   var navbarDiv = $('<nav class="navbar" ></nav>')
+
+//   var brand = $('<a class="navbar-brand" href="#"></a>')
+
+//   brand.text(title)
+
+//   navbarDiv.append(brand)
+
+//   var nav = $('<ul class="navbar-nav">')
+
+//   nav.append($(links.about))
+//   nav.append($(links.bands))
+//   nav.append($(links.contact))
+//   nav.append($(links.shop))
+//   nav.append($(links.sponsorship))
+
+//   navbarDiv.append(nav)
+//   return navbarDiv
+// }
